dcaa8385be5824d17ca728025b055b9f593a4017
Seid Geniyu <seyageniyu@gmail.com>
2024-02-27 12:22:27 +0300

    first commit

---
+++ hello.js
@@ -0,0 +1,23 @@
+let message = "Hello, World!";
+document.getElementById("result").innerHTML = message;
+console.log(message);
+// declaring variables,assigning values to them and performing some basic opertions
+let a = 10;
+let b = 5;
+
+let sum = a + b;
+let difference = a - b;
+let product = a * b;
+let quotient = a / b;
+
+console.log(sum); // Output: 15
+console.log(difference); // Output: 5
+console.log(product); // Output: 50
+console.log(quotient); // Output: 2
+
+//concatinating strings
+let firstName = "Seid";
+let lastName = "Geniyu";
+
+let fullName = firstName + " " + lastName;
+console.log(fullName);
